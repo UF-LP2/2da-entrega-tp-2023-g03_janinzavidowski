de96ebb656c770ef9afed8d2a7de4f499a0d8ed3
emiliajanin <emiliajanin@gmail.com>
2023-10-29 18:30:28 -0300

    Test Enfermero

---
+++ test/TestingEnfermero.py
@@ -0,0 +1,17 @@
+from Enfermero import cEnfermero
+from Paciente import cPaciente
+
+def test_AsignarColor():
+
+    paciente1 = cPaciente(2321, "Emilia", "Janin", "Coma", 21.30, 21.40, "Naranja", 10, 30)
+    enf1 = cEnfermero(2273, "Camila", "Zavidowski", 16)
+
+    enf1.AsignarColor(paciente1)
+
+    assert paciente1.color=="Naranja"
+    assert paciente1.puntos==30
+
+
+
+
+
