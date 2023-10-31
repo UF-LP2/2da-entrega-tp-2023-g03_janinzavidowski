af8e8036b808011194f2e469d1e4d2d51419b453
czavidowski <127987006+czavidowski@users.noreply.github.com>
2023-10-31 16:15:10 -0300

    main

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main() -> None:
-  print("Hello World")
+from library.Enfermero import cEnfermero
+from library.Paciente import cPaciente
+from library.archivo import cargar_pacientes_desde_csv
 
-if __name__ == "__main__":
-  main()
+def main() -> None:
+  lista_pacientes_archivo = cargar_pacientes_desde_csv("pacientes.csv")
+  # Seleccionar un paciente al azar
+  paciente_azar = seleccionar_paciente_azar(lista_pacientes_archivo)
+
+  # Llamar a la función OrdenarGreedy con el paciente seleccionado
+  enfermero.OrdenarGreedy(listapacientes, paciente_azar)
+if __name__ == '__main__':
